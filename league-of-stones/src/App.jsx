41a88a6894db08cf845a7c4b71082e587c9ae8ce
augui <auguiman31@gmail.com>
2022-04-15 14:41:37 +0200

    signin update

--- league-of-stones/src/App.jsx
+++ league-of-stones/src/App.jsx
@@ -5,33 +5,33 @@ import Signin from './Signin'
 import Main from './Main'
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-dark bg-dark">
-          <div className="container-fluid"></div>
-          <ul>
-            <li>
-              <Link className="navbar-brand" to="/main">League of Stone</Link>
-            </li>
-            <li>
-              <Link className="navbar-brand" to="/login">Connexion</Link>
-            </li>
-            <li>
-              <Link className="navbar-brand" to="/signin">Inscription</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-        <Route path="/main" element={<Main/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/signin" element={<Signin/>}/>
-        </Routes>
-        
-      </div>
-    </Router>
+    return (
+        <Router>
+            <div>
+                <nav className="navbar navbar-dark bg-dark">
+                    <div className="container-fluid"></div>
+                    <ul>
+                        <li>
+                            <Link className="navbar-brand" to="/main">League of Stone</Link>
+                        </li>
+                        <li>
+                            <Link className="navbar-brand" to="/login">Connexion</Link>
+                        </li>
+                        <li>
+                            <Link className="navbar-brand" to="/signin">Inscription</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path="/main" element={<Main/>} />
+                    <Route path="/login" element={<Login/>} />
+                    <Route path="/signin" element={<Signin/>}/>
+                </Routes>
 
-  );
+            </div>
+        </Router>
+
+    );
 }
 
 
--- league-of-stones/src/Signin.jsx
+++ league-of-stones/src/Signin.jsx
@@ -1,75 +1,120 @@
-import {Field, Form} from "react-final-form"
+import {Field, Form} from "react-final-form";
 
 function Signin() {
 
     const onSubmit = values => {
-        if(values.name === undefined|| values.password === undefined|| values.mdp_confirm === undefined) {
-            alert("Vous devez remplir tous les champs")
+
+        if(values.name === undefined || values.password === undefined || values.passwordConfirm === undefined) {
+
+            alert("Au moins un des champs est vide");
+
         } else {
-            if (values.pseudo.trim() === "") {
-                alert("entrez un pseudo")
-            } else if (values.mdp.trim() === "") {
-                alert("entrez un mot de passe") 
-            } else if (values.mdp_confirm.trim() === "") {
-                alert("veuillez confirmé le mot de passe")
-            } else if (values.mdp !== values.mdp_confirm) {
-                alert("Les mots de passe ne correspondent pas")
+
+            if(values.name.trim() === "") {
+
+                alert("Veuillez rentrer un pseudo");
+
+            } else if(values.password.trim() === "") {
+
+                alert("Veuillez rentrer un mot de passe");
+
+            } else if(values.passwordConfirm.trim() === "") {
+
+                alert("Veuillez confirmer votre mot de passe");
+
+            } else if(values.password !== values.passwordConfirm) {
+
+                alert("Les mots de passe ne concordent pas");
+
             } else {
-                const newVal = {"name": values.name, "email": values.name +"@L3.fr", "password": values.mdp }
-                fetch ('http://localhost:3001/user', {
+
+                const newVal = {"name": values.name, "email": values.name + "@l3.fr", "password": values.password};
+                fetch('http://localhost:3001/user', {
+
                     method: 'PUT',
                     headers: {
-                        'content-type': 'application/json'
+
+                        'Content-Type': 'application/json',
+
                     },
                     body: JSON.stringify(newVal),
-                }
-                ).then(response => response.json()).then(() => {
-                    document.querySelector("#form").innerHTML = "<h1>votre compte a ete crée</h1>"
-                }).catch((error ) => {
+
+                }).then(response => response.json()).then(() => {
+
+                    document.querySelector("#form").innerHTML =
+                        "<h1>Votre compte à bien été créé</h1>"
+
+                }).catch((error) => {
+
                     console.error('Error:', error);
+
                 });
+
             }
+
         }
+
     }
+
     return (
+
         <Form
+
             onSubmit={onSubmit}
             render={({ handleSubmit }) => (
-                <>
-                <h1>S'inscrire</h1>
-                <div className="container" id="Form">
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="htmlFor" className="form-label">Pseudo</label>
-                            <Field
-                                name="pseudo"
-                                component="input"
-                                className="form-control"
-                                id="pseudo"
-                            />
-                            <label htmlFor="mdp" className="form-label">Mot de passe</label>
-                            <Field
-                                name="mdp"
-                                component="input"
-                                type="password"
-                                className="form-control"
-                                id="mdp"
-                            />
-                            <label htmlFor="mdp" className="form-label">Confirm Mot de passe</label>
-                            <Field
-                                name="mdp_confirm"
-                                component="input"
-                                type="password"
-                                className="form-control"
-                                id="mdp_confirm"
-                            />                              
-                        </div>
-                        <button type="submit" className="btn btn-outline-danger">Connect</button>
-                    </form>
+
+                <div id="signin">
+                    <h1 id="titleForm">S'inscrire</h1>
+                    <div className="container" id="form">
+
+                        <form onSubmit={handleSubmit}>
+
+                            <div className="mb-3">
+
+                                <label htmlFor="name" className="form-label">Pseudo</label>
+                                <Field
+
+                                    name="name"
+                                    component="input"
+                                    className="form-control"
+                                    id="name"
+
+                                />
+                                <label htmlFor="password" className="form-label">Mot de passe</label>
+                                <Field
+
+                                    name="password"
+                                    component="input"
+                                    type="password"
+                                    className="form-control"
+                                    id="password"
+
+                                />
+                                <label htmlFor="passwordConfirm" className="form-label">Confirmez votre mot de passe</label>
+                                <Field
+
+                                    name="passwordConfirm"
+                                    component="input"
+                                    type="password"
+                                    className="form-control"
+                                    id="passwordConfirm"
+
+                                />
+
+                            </div>
+                            <button type="submit" className="btn btn-outline-danger">Inscription</button>
+
+                        </form>
+
+                    </div>
                 </div>
-                </>
+
             )}
+
         />
+
     );
+
 }
-export default Signin;
+
+export default Signin
